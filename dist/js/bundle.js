b3482724ad9a52ff93eeae4104f43cccbebbe46f
ktr92 <kudashevtim@gmail.com>
2023-01-17 10:12:12 +0300

    index

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,3 +1,101 @@
-/*! For license information please see bundle.js.LICENSE.txt */
-(()=>{var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};(()=>{function e(e,t){$(document).click((function(a){$(a.target).closest(`${e},${t}`).length||($(t).removeClass("active"),$(e).removeClass("active"))})),$(document).keyup((function(a){"Escape"===a.key&&($(t).removeClass("active"),$(e).removeClass("active"))}))}$(document).ready((function(){$("[data-toggleclick]").on("click",(function(e){$(this).toggleClass("active"),e.preventDefault();let t=$(this).data("toggleclick");$("[data-toggle].active").not($(`[data-toggle=${t}]`)).removeClass("active"),$("[data-toggleclick].active").not($(`[data-toggleclick=${t}]`)).removeClass("active"),$(`[data-toggle=${t}]`).toggleClass("active"),$(`[data-toggleactive=${t}]`).toggleClass("active")})),$("[data-scroll]").click((function(e){e.preventDefault();let t=$(this).data("scroll");$(this).siblings(`[data-scrollblock=${t}]`).animate({scrollLeft:"+=126px"})})),$("[data-menutoggle]").on("click",(function(e){e.preventDefault();let t=$(this).data("menutoggle");$(`[data-menu=${t}]`).toggleClass("active"),$(this).toggleClass("active"),$(".jsbackdrop").toggleClass("active")})),$(".jsbackdrop").on("click",(function(e){$(this).removeClass("active"),$("[data-menu]").removeClass("active"),$("[data-menutoggle]").removeClass("active")}))})),window.addEventListener("load",(function(){e('[data-toggle="accountmenu"]','[data-toggleclick="accountmenu"]'),e('[data-toggle="messageblock"]','[data-toggleclick="messageblock"]'),e(".fdropdown__menu",".fdropdown__button")}))})(),(()=>{"use strict";e.r(t)})()})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+function initFE() {
+    closeByClickOutside('[data-toggle="accountmenu"]', '[data-toggleclick="accountmenu"]')
+    closeByClickOutside('[data-toggle="messageblock"]', '[data-toggleclick="messageblock"]')
+    closeByClickOutside('.fdropdown__menu', '.fdropdown__button')
+    closeByClickOutside('.headersearch', '[data-toggleclick="headersearch"]')
+    closeByClickOutside('.suggestions', '.searchinput')
+}
+
+function showSuggestions(e) {
+    if (e.value.length > 0) {
+        $(e).closest('[data-togglewrapper]').find('[data-toggle="suggestions"]').addClass('active')
+        $(e).closest('[data-togglewrapper]').addClass('sugg')
+    }
+    else {
+       
+        $(e).closest('[data-togglewrapper]').find('[data-toggle="suggestions"]').removeClass('active')
+        $(e).closest('[data-togglewrapper]').removeClass('sugg')
+    }
+}
+
+function closeByClickOutside(element, button, callback) {
+    $(document).click(function(event) {
+        if (!$(event.target).closest(`${element},${button}`).length) {
+            $(button).removeClass('active')
+            $(element).removeClass('active')
+        }
+    });
+    
+    $(document).keyup(function(e) {
+        if (e.key === "Escape") { // escape key maps to keycode `27`
+            $(button).removeClass('active')
+            $(element).removeClass('active')
+        }
+    });
+
+    if (callback instanceof Function) { callback(); }
+  }
+
+
+function resizeEvents() {
+    $('.siteheader__left').css('min-width', $('.siteheader__right').width())
+}
+
+$( window ).resize(function() {
+    resizeEvents()
+})
+$(document).ready(function() {
+    resizeEvents()
+    $('[data-toggleclick]').on('click', function(e) {
+        $(this).toggleClass('active')
+        e.preventDefault()
+        let dropdown = $(this).data('toggleclick')
+        $('[data-toggle].active').not($(`[data-toggle=${dropdown}]`)).removeClass('active')
+        $('[data-toggleclick].active').not($(`[data-toggleclick=${dropdown}]`)).removeClass('active')
+        $(`[data-toggle=${dropdown}]`).toggleClass('active')
+        $(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+    })
+
+    $('[data-scroll]').click(function(e) {
+        e.preventDefault()
+        let scroll = $(this).data('scroll')
+        $(this).siblings(`[data-scrollblock=${scroll}]`).animate({
+            scrollLeft: '+=126px'
+        });
+    });
+
+    $('[data-menutoggle]').on('click', function(e) {
+        e.preventDefault()
+        let menu = $(this).data('menutoggle')
+        $(`[data-menu=${menu}]`).toggleClass('active')
+        $(this).toggleClass('active')
+        $('.jsbackdrop').toggleClass('active')
+  
+    })
+    $('.jsbackdrop').on('click', function(e) {
+        $(this).removeClass('active')
+        $('[data-menu]').removeClass('active')
+        $('[data-menutoggle]').removeClass('active')
+  
+    })
+});
+
+window.addEventListener('load', function () {
+    initFE()
+});
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,85 @@
-function initFE(){closeByClickOutside('[data-toggle="accountmenu"]','[data-toggleclick="accountmenu"]'),closeByClickOutside('[data-toggle="messageblock"]','[data-toggleclick="messageblock"]'),closeByClickOutside(".fdropdown__menu",".fdropdown__button")}function showSuggestions(t){t.value.length>0?($(t).closest("[data-togglewrapper]").find('[data-toggle="suggestions"]').addClass("active"),$(t).closest("[data-togglewrapper]").addClass("sugg")):($(t).closest("[data-togglewrapper]").find('[data-toggle="suggestions"]').removeClass("active"),$(t).closest("[data-togglewrapper]").removeClass("sugg"))}function closeByClickOutside(t,e){$(document).click((function(a){$(a.target).closest(`${t},${e}`).length||($(e).removeClass("active"),$(t).removeClass("active"))})),$(document).keyup((function(a){"Escape"===a.key&&($(e).removeClass("active"),$(t).removeClass("active"))}))}$(document).ready((function(){$("[data-toggleclick]").on("click",(function(t){$(this).toggleClass("active"),t.preventDefault();let e=$(this).data("toggleclick");$("[data-toggle].active").not($(`[data-toggle=${e}]`)).removeClass("active"),$("[data-toggleclick].active").not($(`[data-toggleclick=${e}]`)).removeClass("active"),$(`[data-toggle=${e}]`).toggleClass("active"),$(`[data-toggleactive=${e}]`).toggleClass("active")})),$("[data-scroll]").click((function(t){t.preventDefault();let e=$(this).data("scroll");$(this).siblings(`[data-scrollblock=${e}]`).animate({scrollLeft:"+=126px"})})),$("[data-menutoggle]").on("click",(function(t){t.preventDefault();let e=$(this).data("menutoggle");$(`[data-menu=${e}]`).toggleClass("active"),$(this).toggleClass("active"),$(".jsbackdrop").toggleClass("active")})),$(".jsbackdrop").on("click",(function(t){$(this).removeClass("active"),$("[data-menu]").removeClass("active"),$("[data-menutoggle]").removeClass("active")}))})),window.addEventListener("load",(function(){initFE()}));
+function initFE() {
+    closeByClickOutside('[data-toggle="accountmenu"]', '[data-toggleclick="accountmenu"]')
+    closeByClickOutside('[data-toggle="messageblock"]', '[data-toggleclick="messageblock"]')
+    closeByClickOutside('.fdropdown__menu', '.fdropdown__button')
+    closeByClickOutside('.headersearch', '[data-toggleclick="headersearch"]')
+    closeByClickOutside('.suggestions', '.searchinput')
+}
+
+function showSuggestions(e) {
+    if (e.value.length > 0) {
+        $(e).closest('[data-togglewrapper]').find('[data-toggle="suggestions"]').addClass('active')
+        $(e).closest('[data-togglewrapper]').addClass('sugg')
+    }
+    else {
+       
+        $(e).closest('[data-togglewrapper]').find('[data-toggle="suggestions"]').removeClass('active')
+        $(e).closest('[data-togglewrapper]').removeClass('sugg')
+    }
+}
+
+function closeByClickOutside(element, button, callback) {
+    $(document).click(function(event) {
+        if (!$(event.target).closest(`${element},${button}`).length) {
+            $(button).removeClass('active')
+            $(element).removeClass('active')
+        }
+    });
+    
+    $(document).keyup(function(e) {
+        if (e.key === "Escape") { // escape key maps to keycode `27`
+            $(button).removeClass('active')
+            $(element).removeClass('active')
+        }
+    });
+
+    if (callback instanceof Function) { callback(); }
+  }
+
+
+function resizeEvents() {
+    $('.siteheader__left').css('min-width', $('.siteheader__right').width())
+}
+
+$( window ).resize(function() {
+    resizeEvents()
+})
+$(document).ready(function() {
+    resizeEvents()
+    $('[data-toggleclick]').on('click', function(e) {
+        $(this).toggleClass('active')
+        e.preventDefault()
+        let dropdown = $(this).data('toggleclick')
+        $('[data-toggle].active').not($(`[data-toggle=${dropdown}]`)).removeClass('active')
+        $('[data-toggleclick].active').not($(`[data-toggleclick=${dropdown}]`)).removeClass('active')
+        $(`[data-toggle=${dropdown}]`).toggleClass('active')
+        $(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+    })
+
+    $('[data-scroll]').click(function(e) {
+        e.preventDefault()
+        let scroll = $(this).data('scroll')
+        $(this).siblings(`[data-scrollblock=${scroll}]`).animate({
+            scrollLeft: '+=126px'
+        });
+    });
+
+    $('[data-menutoggle]').on('click', function(e) {
+        e.preventDefault()
+        let menu = $(this).data('menutoggle')
+        $(`[data-menu=${menu}]`).toggleClass('active')
+        $(this).toggleClass('active')
+        $('.jsbackdrop').toggleClass('active')
+  
+    })
+    $('.jsbackdrop').on('click', function(e) {
+        $(this).removeClass('active')
+        $('[data-menu]').removeClass('active')
+        $('[data-menutoggle]').removeClass('active')
+  
+    })
+});
+
+window.addEventListener('load', function () {
+    initFE()
+});
