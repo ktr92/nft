f70c4f0f8e972256ba8938090a021cb7c7a43fba
ktr92 <kudashevtim@gmail.com>
2023-01-17 10:31:07 +0300

    index

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,7 +1,26 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 (() => {
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
 function initFE() {
     closeByClickOutside('[data-toggle="accountmenu"]', '[data-toggleclick="accountmenu"]')
     closeByClickOutside('[data-toggle="messageblock"]', '[data-toggleclick="messageblock"]')
@@ -92,6 +111,10 @@ window.addEventListener('load', function () {
 // This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
 "use strict";
+/*!*****************************!*\
+  !*** ./src/scss/style.scss ***!
+  \*****************************/
+__webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
 })();
